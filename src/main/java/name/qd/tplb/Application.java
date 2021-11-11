79a3dbaf1404e0e3f8a9ddd55b96b2156e3a8ef2
Shawn <shawn.chou@nogle.com>
2021-11-11 18:37:02 +0800

    1. init project

---
+++ src/main/java/name/qd/tplb/Application.java
@@ -0,0 +1,11 @@
+package name.qd.tplb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+	public static void main(String[] s) {
+		SpringApplication.run(Application.class, s);
+	}
+}
